2bd29c0ec572a73add65f491ae8f284a8eed4b2f
fromwhence <wenc.matt@gmail.com>
2019-10-31 15:22:45 -0500

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,37 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import OpenHouseIndex from '../views/Index.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+ {path: '/', name: 'home', component: OpenHouseIndex },
 ]
-
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+ mode: 'history',
+ base: process.env.BASE_URL,
+ routes
 })
 
 export default router
+
+// import Vue from 'vue'
+// import VueRouter from 'vue-router'
+// import FoodsIndex from '../views/FoodsIndex.vue'
+// import FoodsNew from '../views/FoodsNew.vue'
+// import FoodsShow from '../views/FoodsShow.vue'
+// import FoodsEdit from '../views/FoodsEdit.vue'
+// Vue.use(VueRouter)
+// const routes = [
+//  {path: '/', name: 'home', component: FoodsIndex },
+//  {path: '/foods', name: 'foods-index', component: FoodsIndex },
+//  {path: '/foods/new', name: 'foods-new', component: FoodsNew },
+//  {path: '/foods/:id', name: 'foods-show', component: FoodsShow },
+//  {path: '/foods/:id/edit', name: 'foods-edit', component: FoodsEdit }
+// ]
+// const router = new VueRouter({
+//  mode: 'history',
+//  base: process.env.BASE_URL,
+//  routes
+// })
+// export default router
---
+++ src/views/Index.vue
@@ -0,0 +1,49 @@
+<template>
+  
+  <div class="new-form-section">
+    <h1>Open House Search</h1>
+
+    <form v-on:submit.prevent="search()">
+      Zip Code: <input type="number" v-model="searchTerm" placeholder="Search by Name">
+<!--       Date: <input type="date" value="Search" v-model="searchTerm" placeholder="Search by Name"> -->
+      <input type="submit" value="Submit">
+    </form>
+  </div>
+
+</template>
+
+<style>
+</style>
+
+<script>
+var axios = require('axios');
+
+export default {
+  data: function() {
+    return {
+      open_house: [],
+      searchTerm: ""
+    };
+  },
+  created: function() {
+    axios
+      .get("/api/open_houses")
+      .then(response => {
+        this.open_houses = response.data;
+      });
+  },
+  methods: {
+     search: function() {
+       axios
+         .get("/api/foods?search=" + this.searchTerm)
+         .then(response => {
+           this.foods = response.data;
+         });
+     }
+   }
+  };
+  </script>
+};
+</script>
+
+
